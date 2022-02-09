76f05ebbe2a688016114bbba1ed533e1be0e25ce
kumudinidesale <kdesale1211@gmail.com>
2022-02-09 13:07:07 +0530

    added:Gambler is win or lose

--- src/com/java/GamblingSimulator.java
+++ src/com/java/GamblingSimulator.java
@@ -1,12 +1,21 @@
 package com.java;
-
+import java.util.Scanner;
 public class GamblingSimulator {
 
 	public static void main(String[] args) {
 	System.out.println("Welcome to gambling simulator");
 	int stake =100;
 	int bet =1;
-
+     int GAME =(int) Math.floor(Math.random() * 2);
+     if (GAME == 0) {
+    	 stake++;
+     System.out.println("Gambler won the game ");
+     System.out.println("Gambler having stake after bet $ " +stake);
+     }
+	else {
+		stake--;
+		System.out.println("Gambler lose the game");
+	     System.out.println("Gambler having stake after bet $ " +stake);
 	}
-
+}
 }
